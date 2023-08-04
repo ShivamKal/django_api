0959738e97f7f62a97013b54fa0f2a97756919a6
Shivam <shivam.singh@kaleyra.com>
2023-08-04 15:10:23 +0530

    checking

--- api/models.py
+++ api/models.py
@@ -2,13 +2,11 @@ from django.db import models
 import time
 
 class User(models.Model):
-
     user_id = models.CharField(primary_key=True, default=str(int(time.time())), max_length=12, editable=False)
     user_data = models.JSONField(default=dict)
 
 
 class Flag(models.Model):
-
     user_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     user_flags = models.JSONField(default=dict)
 
